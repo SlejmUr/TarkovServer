2bdb6c5602d9a4609b593a9b0fe397d784d3b655
SlejmUr <slejmur@protonmail.com>
2022-07-19 14:18:01 +0200

    Added WebLogin

---
+++ Tarkov_Server_Csharp/Web/WebLogin.cs
@@ -0,0 +1,44 @@
+﻿using HttpServerLite;
+
+namespace Tarkov_Server_Csharp.Web
+{
+    internal class WebLogin
+    {
+        /*
+        Currently this ?u=x&m=x&e=eod&p=x NOT Working
+        in BODY send this:
+        {
+            "username": "YOURNAME",
+            "email": "YOURNAME",
+	        "password": "YOURPASSWORD",
+            "edition": "Edge Of Darkness",
+        }
+        and will work!
+        */
+        [StaticRoute(HttpServerLite.HttpMethod.POST, "/webprofile/login")]
+        public virtual async Task GameStart(HttpContext ctx)
+        {
+            Console.WriteLine(ctx.Request.ToJson(true));
+            // RPS
+            string resp = Controllers.AccountController.Login(ctx.Request.DataAsString);
+            ctx.Response.StatusCode = 200;
+            ctx.Response.ContentType = "text/plain";
+            ctx.Response.ContentLength = resp.Length;
+            await ctx.Response.TrySendAsync(resp);
+            return;
+        }
+        [StaticRoute(HttpServerLite.HttpMethod.POST, "/webprofile/register")]
+        public virtual async Task LauncherRegister(HttpContext ctx)
+        {
+            //REQ stuff
+            Console.WriteLine(ctx.Request.ToJson(true));
+            // RPS
+            string resp = Controllers.AccountController.Register(ctx.Request.DataAsString);
+            ctx.Response.StatusCode = 200;
+            ctx.Response.ContentType = "text/plain";
+            ctx.Response.ContentLength = resp.Length;
+            await ctx.Response.SendWithoutCloseAsync(resp);
+            return;
+        }
+    }
+}
