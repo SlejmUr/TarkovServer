04e95b3a75fd878ec868c47fa0dc65e39cb9d243
SlejmUr <slejmur@protonmail.com>
2022-08-02 21:03:01 +0200

    started basic CharCon.

---
+++ ServerLib/Controllers/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ServerLib.Utilities;
+
+namespace ServerLib.Controllers
+{
+    public class CharacterController 
+    {
+        public static List<Json.Character> Character;
+        public static List<Json.Character> ScavCharacter;
+
+        public static void Init()
+        {
+            Character = new();
+            Character.Clear();
+            ScavCharacter = new();
+            ScavCharacter.Clear();
+            Utils.PrintDebug("Initialization Done!", "debug", "[CHARACTER]");
+        }
+
+        public static string GetCompleteCharacter(string sessionID)
+        {
+            List<string> ouptut = new();
+
+            if (!AccountController.IsWiped(sessionID))
+            {
+                //ouptut.Add(JsonConvert.SerializeObject(Character[sessionID].ToString()));
+                //ouptut.Add(JsonConvert.SerializeObject(ScavCharacter[sessionID].ToString()));
+            }
+
+            return JsonConvert.SerializeObject(ouptut);
+        }
+    }
+}
