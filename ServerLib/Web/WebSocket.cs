e21cce53864d9c78232915c7ab08cadc2d5f37bb
SlejmUr <slejmur@protonmail.com>
2023-10-16 22:42:58 +0200

    only websokcet now good, soon more

--- ServerLib/Web/WebSocket.cs
+++ ServerLib/Web/WebSocket.cs
@@ -1,9 +1,9 @@
-﻿using System.Net.Sockets;
-using System.Text;
+﻿using System.Reflection;
+using ModdableWebServer;
+using ModdableWebServer.Attributes;
+using ModdableWebServer.Helper;
+using ModdableWebServer.Servers;
 using NetCoreServer;
-using Newtonsoft.Json;
-using ServerLib.Controllers;
-using ServerLib.Utilities;
 
 namespace ServerLib.Web
 {
@@ -11,90 +11,47 @@ namespace ServerLib.Web
     {
         public static string IpPort = "ws://127.0.0.1:444/";
         public static string IP = "127.0.0.1:444";
-        public static List<string> ConnectedIps = new();
-        public static Dictionary<Guid, string> GuidIP = new();
-        public static Dictionary<string, Guid> IPToGuid = new();
-        public static Dictionary<string, string> ConnectedSessions = new();
-        public static EventHandler<byte[]> MessageReceivedEvent = null;
-        static TarkovWSServer SocketServer;
+        static WSS_Server? WSS_Server = null;
+        static WS_Server? WS_Server = null;
         public static void Start(string ip, int port, bool ssl = true)
         {
             if (ssl)
             {
-                Debug.PrintWarn("Currently WSS not supperted! Using WS");
+                var cert = Utilities.Helpers.CertHelper.GetCert();
+                SslContext context = new(System.Security.Authentication.SslProtocols.Tls12, cert);
+                WSS_Server = new(context, ip, port);
+                WSS_Server.WS_AttributeToMethods.Merge(Assembly.GetAssembly(typeof(WebSocket)));
+                WSS_Server.Start();
             }
-            //IpPort = ssl ? $"wss://{ip}:{port}/socket/" : $"ws://{ip}:{port}/socket/";
-            IpPort = $"ws://{ip}:{port}/";
+            else 
+            {
+                WS_Server = new(ip, port);
+                WS_Server.WS_AttributeToMethods.Merge(Assembly.GetAssembly(typeof(WebSocket)));
+                WS_Server.Start();
+            }
+            IpPort = ssl ? $"wss://{ip}:{port}/socket/" : $"ws://{ip}:{port}/socket/";
             IP = $"{ip}:{port}";
-            SocketServer = new(ip, port);
-            SocketServer.Start();
             Console.WriteLine("WebSocket Server started on " + IpPort);
         }
+
         public static void Stop()
         {
-            if (SocketServer != null)
+            if (WS_Server != null)
             {
-                SocketServer.Stop();
-                SocketServer.Dispose();
+                WS_Server.Stop();
+                WS_Server = null;
+            }
+            if (WSS_Server != null)
+            {
+                WSS_Server.Stop();
+                WSS_Server = null;
             }
         }
 
-        public static TarkovWSServer? GetServer()
+        [WS("/socket/{id}")]
+        public static void WSControl(WebSocketStruct webSocket)
         {
-            if (SocketServer != null && !SocketServer.IsDisposed)
-            {
-                return SocketServer;
-            }
-            return null;
-        }
-
-        public class TarkovSession : WsSession
-        {
-            public TarkovSession(WsServer server) : base(server) { }
-
-            public override void OnWsConnected(HttpRequest request)
-            {
-                Console.WriteLine($"WebSocket session with Id {Id} connected!");
-                Debug.PrintDebug(request.ToString());
-                Debug.PrintDebug(request.Url);
-                //Server.Multicast(JsonConvert.SerializeObject(NotificationController.DefaultNotification(), Formatting.Indented));
-            }
-
-            public override void OnWsDisconnected()
-            {
-                Console.WriteLine($"WebSocket session with Id {Id} disconnected!");
-            }
-
-            public override void OnWsReceived(byte[] buffer, long offset, long size)
-            {
-                try
-                {
-                    string message = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
-                    Console.WriteLine("Incoming: " + message);
-                }
-                catch
-                {
-                    Console.WriteLine("Incoming: " + BitConverter.ToString(buffer));
-                }
-
-            }
-
-            protected override void OnError(SocketError error)
-            {
-                Console.WriteLine($"WebSocket session caught an error with code {error}");
-            }
-        }
-
-        public class TarkovWSServer : WsServer
-        {
-            public TarkovWSServer(string address, int port) : base(address, port) { }
-
-            protected override TcpSession CreateSession() { return new TarkovSession(this); }
-
-            protected override void OnError(SocketError error)
-            {
-                Console.WriteLine($"Tarkov WebSocket server caught an error with code {error}");
-            }
+            Console.WriteLine("websocket hit!");
         }
     }
 }
